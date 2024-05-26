91e7db7454e01eed9de3af9538583968be1ae051
sudhakar961612 <s3xhdhh@gmail.com>
2024-05-26 16:41:25 +0530

    intial

--- index.js
+++ index.js
@@ -4,56 +4,62 @@ const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = data.products;
 
 const express = require("express");
-
 const morgan = require("morgan");
-
 const server = express();
 
-server.use(morgan("default"));
-
-// server.use((req, res, next) => {
-//   console.log(req.method, req.ip, req.hostname, new Date(), req.get("User-Agent"));
-//   next();
-// });
-
 // bodyParser
+server.use(morgan("default"));
 server.use(express.json());
-
-// server.use(express.urlencoded());
-
 server.use(express.static("public")); // stati middleware
-
-const auth = (req, res, next) => {
-  // console.log(req.query);
-
-  // if (req.body.password == "123") {
-  //   next();
-  // } else {
-  //   res.sendStatus(401);
-  // }
-
-  next();
-};
-
-// server.use(auth; application middleware
 
 //API - Endpoint -Route
 
-server.get("/product/:id", auth, (req, res) => {
-  console.log(req.params);
-  res.json({ type: "GET" });
+// Products
+// API ROOT, BASE URL /rest api
+
+// Create POST /products C R U D
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body);
 });
-server.post("/", auth, (req, res) => {
-  res.json({ type: "POST" });
+
+// Read GET /products
+server.get("/products", (req, res) => {
+  res.json(products);
 });
-server.put("/", (req, res) => {
-  res.json({ type: "PUT" });
+
+//Read Get /products/:id
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const product = products.find((p) => p.id === id);
+  res.json(product);
 });
-server.delete("/", (req, res) => {
-  res.json({ type: "DELETE" });
+
+// Update PUT /products/:id
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  products.splice(productIndex, 1, { ...req.body, id: id });
+  res.status(201).json();
 });
-server.patch("/", (req, res) => {
-  res.json({ type: "PATCH" });
+
+// Update PATCH /products/:id
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1, { ...product, ...req.body });
+  res.status(201).json();
+});
+
+// Delete DELETE /products/:id
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1);
+  res.status(201).json(product);
 });
 
 server.get("/demo", (req, res) => {
